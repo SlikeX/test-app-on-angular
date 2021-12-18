a5eff679438b20f042195b0654ad0b538c966216
SlikeX <thelastslike@gmail.com>
2021-12-18 13:02:05 +0500

    init:add project on git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { StartMainComponent } from './start-main/start-main.component';
+import { ToysMainComponent } from './toys-main/toys-main.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    StartMainComponent,
+    ToysMainComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
